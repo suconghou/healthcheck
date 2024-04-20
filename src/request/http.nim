1ad16f7bec2dad0a4f0d776c4ef9cae7be0df96f
suconghou <1126045770@qq.com>
2024-04-20 18:35:18 +0800

    fix: add report

--- src/request/http.nim
+++ src/request/http.nim
@@ -1,22 +1,30 @@
-import strutils
-import httpClient
+import strutils, httpClient
 
-proc get*(url: string, timeout: int, str: string): bool =
-    let client = newHttpClient(timeout = timeout)
-    let resp = client.request(url)
-    if resp.code == Http200:
-        if str != "":
-            return resp.body.contains(str)
-        return true
-    return false
+proc req(kv: openArray[tuple[key: string, val: string]], ua: string = "", refer: string = ""): HttpHeaders =
+    var headers = newHttpHeaders(kv)
+    if not ua.isEmptyOrWhitespace:
+        headers.add("User-Agent", ua)
+    if not refer.isEmptyOrWhitespace:
+        headers.add("Referer", refer)
+    return headers
 
-proc post(url: string, timeout: int, body: string): bool =
-    let headers = newHttpHeaders({"Content-Type": "application/json"})
+proc get*(url: string, timeout: int, ua: string = "", refer: string = ""): (HttpCode, string) =
     let client = newHttpClient(timeout = timeout)
-    let resp = client.request(url, HttpPost, body, headers)
+    let resp = client.request(url, headers = req([], ua, refer))
+    return (resp.code, resp.body)
+
+proc post(url: string, timeout: int, body: string, ua: string = "", refer: string = ""): bool =
+    let client = newHttpClient(timeout = timeout)
+    let resp = client.request(url, HttpPost, body, headers = req({"Content-Type": "application/json"}, ua, refer))
     if resp.code == Http200 or resp.code == Http204:
         return true
     return false
+
+proc report*(url: string, body: string, ua: string = "", refer: string = ""): bool =
+    try:
+        return post(url, timeout = 5000, body, ua, refer)
+    except Exception:
+        return false
 
 proc notify*(body: string, tokens: openArray[string], timeout = 5000): seq[bool] =
     var res: seq[bool]
--- src/utils/util.nim
+++ src/utils/util.nim
@@ -1,9 +1,9 @@
 import parseopt, strutils, os, json, times
 
 type Info* = object
-    name: string
-    time: string
-    node: string
+    name*: string
+    time*: string
+    node*: string
 
 
 
@@ -14,23 +14,29 @@ type Config* = object
     match*: string
     file*: string
     tokens*: seq[string]
+    report*: string
+    info*: Info
 
 proc read*(path: string): string =
-    let exist = fileExists(path)
-    var status = ""
-    if exist:
-        status = readFile(path)
-    return status
+    if fileExists(path):
+        return readFile(path)
+    return "0"
 
 
 proc put*(file: string, text: string) =
     writeFile(file, text)
 
 
+proc getinfo(): Info =
+    let name = try: readFile("/etc/hostname") except IOError: getEnv("HOSTNAME")
+    let time = now().format("yyyy-MM-dd HH:mm:ss")
+    let node = getEnv("NODENAME")
+    return Info(name: name, time: time, node: node)
+
 
 proc cmd*(): Config =
     var cfg = Config(name: "", url: "", timeout: 8000, match: "",
-            file: "/dev/shm/status", tokens: @[])
+            file: "/dev/shm/status", tokens: @[], report: "", info: getinfo())
     for kind, key, val in getopt():
         case kind
         of cmdArgument:
@@ -42,17 +48,11 @@ proc cmd*(): Config =
             of "timeout", "t": cfg.timeout = try: parseUint(val) except ValueError: 8000
             of "match", "m": cfg.match = val
             of "file", "f": cfg.file = val
+            of "report", "r": cfg.report = val
         of cmdEnd: assert(false) # cannot happen
     return cfg
 
-proc getinfo*(): Info =
-    let name = try: readFile("/etc/hostname") except IOError: "unknow"
-    let time = now().format("yyyy-MM-dd HH:mm:ss")
-    let node = getEnv("NODENAME")
-    return Info(name: name, time: time, node: node)
-
-proc buildText*(title: string): string =
-    let info = getinfo()
+proc buildText*(title: string, info: Info): string =
     let text = [
         title,
         "容器ID: "&info.name,
